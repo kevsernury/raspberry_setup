bfc3937e3e8a0d2748ac5df763f335b94bb5d670
kevsernury <68063554+kevsernury@users.noreply.github.com>
2023-09-12 11:49:49 -0700

    Update git-push.bash

--- git-push.bash
+++ git-push.bash
@@ -3,7 +3,7 @@ while true
 do 
  cd ~/Desktop/raspberry_sensors/data
  git add .
- git commit -m "ubuntu `date`"
+ git commit -m "raspberryname `date`"
  git push --set-upstream origin ubuntu
  echo "pushing"
  sleep 20

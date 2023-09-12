683806f942a0592d1a8e9b78ee45809e69dcee64
kevsernury <68063554+kevsernury@users.noreply.github.com>
2023-09-12 11:50:05 -0700

    Update set_git_repo.bash

--- set_git_repo.bash
+++ set_git_repo.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/Desktop/data
+cd ~/Desktop/raspberry_sensors/raspberry_setup
 echo "Raspberry Name"
 read raspName
 sed -i "s/raspberryname/${raspName}/" sensors.py
@@ -9,13 +9,15 @@ echo "------------------------Go to github settings and click 'add SSH key' with
 cat ~/.ssh/id_rsa.pub
 echo "-----------------------------------------Press enter after copy.--------------------------------------------"
 read pulse
-sudo apt-get install python pip git
+cd ~/Desktop/raspberry_sensors/data
 git init
-git config --global init.defaultBranch origin $raspName
+#git config --global init.defaultBranch origin main
 git config --global user.email "kevsernuryildiz@hotmail.com.tr"
 git config --global user.name "kevsernury"
 git config --global pull.rebase false
 git remote set-url origin git@github.com:kevsernury/data.git
+echo $raspName
 git branch $raspName
 git checkout $raspName
-git push --set-upstream origin $raspName
+#git push --set-upstream origin $raspName
+#git push -u origin $raspName
